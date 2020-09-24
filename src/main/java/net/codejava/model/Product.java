14732f092c54308594f3900f766e6853523d2ea0
Jose Abraham Ocampo Melgar <jose.ocampom@SofttekClients.com>
2020-09-24 12:50:00 -0500

    fisrt commit

---
+++ src/main/java/net/codejava/model/Product.java
@@ -0,0 +1,62 @@
+package net.codejava.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Product {
+	private Long id;
+	private String name;
+	private String email;
+	private String address;
+
+	public Product() {
+	}
+
+	protected Product(Long id, String name, String email, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
